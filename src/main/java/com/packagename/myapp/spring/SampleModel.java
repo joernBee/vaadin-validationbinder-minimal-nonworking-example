4c57fcf9eae18a745137157d55c1c576bff93e3b
Thilo Kogge <thilo.kogge@trinomica.de>
2019-11-05 14:37:19 +0100

    v13

--- src/main/java/com/packagename/myapp/spring/SampleModel.java
+++ src/main/java/com/packagename/myapp/spring/SampleModel.java
@@ -1,9 +1,5 @@
 package com.packagename.myapp.spring;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
